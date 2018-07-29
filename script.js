536fe34b0baa8b825fe1c60d15c6cb5472486ded
Jacob Silcoff <jacobsilcoff@gmail.com>
2018-07-29 20:51:04 +0000

    Added a page to start an auction

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 let x = document.querySelector("#x")
 let box = document.querySelector('#participatebox')
 let p = document.querySelector("#participate")
-
+let start = document.querySelector("#create")
 p.addEventListener('click',e=>{
     box.classList.remove('hidden')
 })
 
 x.addEventListener('click',e=>{
     box.classList.add('hidden')
+})
+
+start.addEventListener('click', e=>{
+  box.classList.remove('hidden')
 })
 
 function myFunction() {
@@ -23,3 +27,17 @@ function outFunc() {
   var tooltip = document.getElementById("myTooltip");
   tooltip.innerHTML = "Copy to clipboard";
 }
+
+$(document).ready(function(){
+    console.log("READY")
+    $("input#namein").on({
+    keydown: function(e) {
+      if (e.which === 32)
+        return false;
+    },
+    change: function() {
+      this.value = this.value.replace(/\s/g, "")
+      this.value = this.value.toLowerCase()
+    }
+  });
+})
